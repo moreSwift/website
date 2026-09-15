d939f27c9f8348b0ecf044176de8e7dacac8e345
stackotter <stackotter@stackotter.dev>
2026-09-15 13:26:18 +1000

    Add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<style>
+	p {
+		text-align: center;
+		font-size: 1.2rem;
+		margin-top: 3rem;
+	}
+</style>
+
+{#if page.error?.message == "Not Found"}
+	<p>Page not found. <a href="/">Return home?</a></p>
+{:else}
+	<p>{page.error?.message}</p>
+{/if}
