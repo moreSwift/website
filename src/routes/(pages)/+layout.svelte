1178b9bd5ff4c5846b5e6894a5a27f4837b7ec6a
stackotter <stackotter@stackotter.dev>
2026-09-15 13:10:22 +1000

    Update favicon (to moreSwift logo)

--- src/routes/(pages)/+layout.svelte
+++ src/routes/(pages)/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 	import Nav from "$lib/Nav.svelte";
   import "$lib/assets/worksans.css";
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <style>
 	:root {
