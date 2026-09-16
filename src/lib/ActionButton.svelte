87bbff82e774e7402a5a2274f96a2edfc96192b8
stackotter <stackotter@stackotter.dev>
2026-09-16 23:45:36 +1000

    Increase font size of call to action

--- src/lib/ActionButton.svelte
+++ src/lib/ActionButton.svelte
@@ -10,8 +10,14 @@
     border-radius: 0.25rem;
     padding: 0.6rem 1rem;
     font-weight: 500;
-    font-size: 1.2rem;
+    font-size: 1.8rem;
   }
+
+	@media screen and (max-width: 600px) {
+	  a {
+      font-size: 1.2rem;
+	  }
+	}
 </style>
 
 <a href={href} role="button">{title}</a>
